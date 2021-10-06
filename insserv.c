69a6459e4d06e31b3ab0f01048128fc02e01d27c
Jesse Smith <jsmith@resonatingmedia.com>
2021-10-06 08:34:05 -0300

    Fix off-by-one error in filename filtering.
    Patch provided by Mark Hindley.

--- insserv.c
+++ insserv.c
@@ -2281,7 +2281,7 @@ static int cfgfile_filter(const struct dirent *restrict d)
 	    !strcmp(end,  "new")	||
 	    !strcmp(end,  "org")	||
 	    !strcmp(end,  "orig")	||
-	    !strncmp(end, "dpkg", 3)	|| /* .dpkg-old, .dpkg-new ... */
+	    !strncmp(end, "dpkg", 4)	|| /* .dpkg-old, .dpkg-new ... */
 	    !strcmp(end,  "save")	||
 	    !strcmp(end,  "swp")	|| /* Used by vi like editors */
 	    !strcmp(end,  "core"))	   /* modern core dump */
